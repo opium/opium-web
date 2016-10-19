a772658b919ffdc0daefe7cb79acc6ff18584f36
Julien Deniau <julien.deniau@gmail.com>
2016-10-19 13:32:59 +0200

    add missing props to Thumbnail

--- src/Thumbnail.test.js
+++ src/Thumbnail.test.js
@@ -4,5 +4,11 @@ import Thumbnail from './Thumbnail';
 
 it('renders without crashing', () => {
   var  div = document.createElement('div');
-  ReactDOM.render(<Thumbnail />, div);
+  ReactDOM.render(
+    <Thumbnail
+      image="http://demo.opium.sitioweb.fr/2909-vallon-moy-res-jpg/thumbs/200-200"
+      title="Alps"
+    />,
+    div
+  );
 });
