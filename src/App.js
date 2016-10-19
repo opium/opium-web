6d27d70a675ba18aaeb872220a7edb275c0571ad
Julien Deniau <julien.deniau@gmail.com>
2016-10-19 13:22:14 +0200

    use flex, add favicon and normalize.css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import Thumbnail from './Thumbnail';
 class App extends Component {
   render() {
     return (
-      <div className="ThumbnailList">
-        <Thumbnail
-          title="Alps"
-          image="http://demo.opium.sitioweb.fr/2909-vallon-moy-res-jpg/thumbs/200-200"
-        />
-        <Thumbnail
-          title="Pacific Ocean"
-          image="http://demo.opium.sitioweb.fr/2010-mavericks-competition-edit1-jpg/thumbs/200-200"
-        />
-        <Thumbnail
-          title="Venezia"
-          image="http://demo.opium.sitioweb.fr/basilica-di-san-giorgio-maggiore-a-venezia-jpg/thumbs/200-200"
-        />
+      <div>
+        <div className="ThumbnailList">
+          <Thumbnail
+            title="Alps"
+            image="http://demo.opium.sitioweb.fr/2909-vallon-moy-res-jpg/thumbs/200-200"
+          />
+          <Thumbnail
+            title="Pacific Ocean"
+            image="http://demo.opium.sitioweb.fr/2010-mavericks-competition-edit1-jpg/thumbs/200-200"
+          />
+          <Thumbnail
+            title="Venezia"
+            image="http://demo.opium.sitioweb.fr/basilica-di-san-giorgio-maggiore-a-venezia-jpg/thumbs/200-200"
+          />
+        </div>
+
+        <footer className="Footer">
+          Total number of items: 3
+        </footer>
       </div>
     );
   }
