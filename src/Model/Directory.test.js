d2832d176ef7c85cf479c82c55df26bfb7dc3025
Julien Deniau <julien.deniau@gmail.com>
2016-10-23 23:13:23 +0200

    use .toBeInstanceOf in tests

--- src/Model/Directory.test.js
+++ src/Model/Directory.test.js
@@ -9,28 +9,28 @@ import rawDirectoryWithParent from 'rawDirectoryWithParent';
 it('test passing anything to entityFactory', () => {
   const item = entityFactory({ a: 'A' });
 
-  expect(item instanceof Map).toBeTruthy();
+  expect(item).toBeInstanceOf(Map);
   expect(item.get('a')).toEqual('A');
 });
 
 it('generate a valid entity', () => {
   const directory = new Directory(rawDirectory);
 
-  expect(directory instanceof Directory).toBeTruthy();
+  expect(directory).toBeInstanceOf(Directory);
   expect(directory.id).toEqual(1);
   expect(directory.name).toEqual('');
-  expect(directory.children instanceof List).toBeTruthy();
+  expect(directory.children).toBeInstanceOf(List);
   expect(directory.parent).toBeFalsy();
 
   const alps = directory.children.first();
-  expect(alps instanceof Directory).toBeTruthy();
+  expect(alps).toBeInstanceOf(Directory);
   expect(alps.id).toEqual(2);
   expect(alps.name).toEqual('Alps');
 
   const imageLines = directory.imageLines;
-  expect(imageLines instanceof List).toBeTruthy();
+  expect(imageLines).toBeInstanceOf(List);
   expect(imageLines.size).toEqual(1);
-  expect(imageLines.first() instanceof Map).toBeTruthy();
+  expect(imageLines.first()).toBeInstanceOf(Map);
   expect(imageLines.first().size).toEqual(3);
 
   expect(directory.getChildById(2).name).toEqual('Alps');
@@ -40,15 +40,15 @@ it('generate a valid entity', () => {
 it('generate a valid entity with parent', () => {
   const directory = new Directory(rawDirectoryWithParent);
 
-  expect(directory instanceof Directory).toBeTruthy();
-  expect(directory.parent instanceof Directory).toBeTruthy();
+  expect(directory).toBeInstanceOf(Directory);
+  expect(directory.parent).toBeInstanceOf(Directory);
 });
 
 it('generate a valid entity with file as children', () => {
   const directory = new Directory(rawDirectoryWithParent);
 
-  expect(directory.children instanceof List).toBeTruthy();
+  expect(directory.children).toBeInstanceOf(List);
 
   const file = directory.children.first();
-  expect(file instanceof File).toBeTruthy();
+  expect(file).toBeInstanceOf(File);
 });
