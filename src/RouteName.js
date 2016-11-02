85edee382fedf81e48fd8b634264a5ad70c6ad71
Julien Deniau <julien.deniau@gmail.com>
2016-11-02 15:07:35 +0100

    fix routes + title in file path

--- src/RouteName.js
+++ src/RouteName.js
@@ -1,4 +1,4 @@
-export const ROUTE_LOGIN = 'opium/login';
-export const ROUTE_PALETTE = 'opium/palette';
-export const ROUTE_UPLOAD = 'opium/upload/';
+export const ROUTE_LOGIN = '/opium/login';
+export const ROUTE_PALETTE = '/opium/palette';
+export const ROUTE_UPLOAD = '/opium/upload/';
 
