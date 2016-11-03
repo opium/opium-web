214aa58cdaef6618ec5ca45c5cf5dd46730f20d2
Julien Deniau <julien.deniau@gmail.com>
2016-11-03 20:51:22 +0100

    Directory full map

---
+++ src/Component/DirectoryMap.js
@@ -0,0 +1,66 @@
+import React, { Component, PropTypes } from 'react';
+import Helmet from 'react-helmet';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const DirectoryMapInner = ({ bounds, markers, ...rest }) =>
+  <Map
+    bounds={[[bounds.get('top'), bounds.get('left')], [bounds.get('bottom'), bounds.get('right')]]}
+    {...rest}
+  >
+    <TileLayer
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    />
+    {markers.map((marker, i) =>
+      <Marker key={i} position={marker} />
+    )}
+  </Map>
+;
+
+class DirectoryMap extends Component {
+  static propTypes = {
+    directory: PropTypes.object,
+    findDirectory: PropTypes.func.isRequired,
+    slug: PropTypes.string.isRequired,
+    mapHeight: PropTypes.number.isRequired,
+  }
+
+  componentDidMount() {
+    if (!this.props.directory) {
+      this.props.findDirectory(this.props.slug);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.slug !== prevProps.slug) {
+      this.props.findDirectory(this.props.slug);
+    }
+  }
+
+  render() {
+    const { directory } = this.props;
+
+    if (!directory) {
+      return null;
+    }
+
+    const markers = directory.children
+      .filter(child => !!child.position)
+      .map(child => [child.position.get('lat'), child.position.get('lng')]);
+
+
+    return (
+      <div>
+        <Helmet title={`${directory.name} - World Map`} />
+
+        <DirectoryMapInner
+          bounds={directory.getPositionBounds()}
+          markers={markers}
+          style={{ height: this.props.mapHeight }}
+        />
+      </div>
+    );
+  }
+}
+
+export default DirectoryMap;
--- src/RouteName.js
+++ src/RouteName.js
@@ -2,4 +2,3 @@ export const ROUTE_LOGIN = '/opium/login';
 export const ROUTE_PALETTE = '/opium/palette';
 export const ROUTE_UPLOAD = '/opium/upload/';
 export const ROUTE_DIRECTORY_MAP = '/opium/map/';
-
